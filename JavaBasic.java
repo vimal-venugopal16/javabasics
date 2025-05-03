374fde0ba0f07aadabe48b52edb9673919b86113
Vimal Venugopal <deep781@yahoo.com>
2025-05-02 21:12:47 -0500

    Update JavaBasic.java

--- JavaBasic.java
+++ JavaBasic.java
@@ -8,7 +8,7 @@ import java.util.*;
 public class Java1DArray {
 
     public static void main(String[] args) {
-
+        public static string password ="asdsadsadsada"
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
         int[] a = new int[n];
