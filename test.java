d2ad7b0fe4bd22441a90f5863deff3046f447f74
Vimal Venugopal <deep781@yahoo.com>
2025-05-18 13:50:41 -0500

    Create test.java

---
+++ test.java
@@ -0,0 +1,23 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Print a message to the console
+        System.out.println("Hello, World!");
+
+        // Basic variables
+        int number = 42;
+        String name = "Alice";
+
+        // Conditional statement
+        if (number > 10) {
+            System.out.println(name + " has a large number.");
+        } else {
+            System.out.println(name + " has a small number.");
+        }
+
+        // Loop example
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Counting: " + i);
+        }
+    }
+}
